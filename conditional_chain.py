a1bf95fda5417c00946c00a8fd349695a16b3ee6
Muhammad Saad Bin Zubair <163721486+Saadt33@users.noreply.github.com>
2026-08-11 20:17:33 +0500

    Update conditional_chain.py

--- conditional_chain.py
+++ conditional_chain.py
@@ -1,5 +1,5 @@
 from langchain_openai import ChatOpenAI
-from langchain_anthropic import ChatAnthropic 
+from langchain_anthropic import ChatAnthropic  
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
